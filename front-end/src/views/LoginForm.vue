e4d40bbb82149c81aa98fc6dfd9d23a01f6c041d
LilBoii <choreiwken@outlook.com>
2023-07-10 21:43:04 +0700

    27 | Auth Front-End

---
+++ front-end/src/views/LoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input type="email" v-model="email" placeholder="Email" required>
+            <input type="password" v-model="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await fetch('/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: this.email,
+                        password: this.password
+                    })
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    const token = data.token;
+
+                    // Save the token to local storage or Vuex store for future use
+                    localStorage.setItem('token', token);
+
+                    // Assuming the login request was successful, navigate to the home page
+                    this.$router.push('/home');
+                } else {
+                    throw new Error('Invalid email or password');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+};
+</script>
+  
+<style>
+/* Add your custom styles here */
+</style>
+  
